045ecd696fae58d713e20c9889db8c4de7ff48ea
MgBacon <mgcbkmn@googlemail.com>
2016-08-03 20:12:11 +0200

    -added log to file

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var database = require('./server/router/API');
-var auth= require('./server/router/Auth');
+var express=require('express');
+var exphbs=require('express-handlebars');
+var morgan=require('morgan');
+var database=require('./server/router/API');
+var auth=require('./server/router/Auth');
+var fs = require('fs');
+
+
+
+var port=3000;
 
 var app=express();
 app.disable("x-powered-by");
-
-app.use('/api/v1',database);
-app.use('/auth/',auth);
-
 app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//simple logger is simple
-app.use(function (req,resp,next) {
-    console.log(new Date()+ ": URL: "+req.url+" requested by IP:"+req.ip);
-    next();
-});
+//Setting up the loggers
+var access_logfile = fs.createWriteStream('./access.log', {flags: 'a'});
+var type="[:date[clf]] :remote-addr Status: :status Path: :url :response-time ms :res[body]";
+app.use(morgan(type,{stream: access_logfile }));
+app.use(morgan('dev'));
+
+app.use('/api/v1',database);
+app.use('/auth/',auth);
+
 app.get('/', function (req, res) {
     res.render('home');
 });
@@ -51,6 +57,5 @@ app.use(function (error, req,resp) {
     resp.status(500);
     resp.render("500");
 });
-app.listen(3000,function(){
-    console.log("Express has been started.");
-});
+app.listen(port);
+console.log("Express has been started on port "+port);
